4266e723f2056923cbc3d412d4f3b60590201158
Richard Plute <rplute@gmail.com>
2016-08-30 00:18:10 -0700

    refactoring users service

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,30 +8,37 @@ var jwt = require('jsonwebtoken');
 
 module.exports = (function() {
 	return {
-		dummyCreate: function(req, res) {
-				var f = new Users({login:'dummy', password:'password', numTopic: 0, numPost: 0, numComment: 0});
+		dummyCreate: function(req, res, next) {
+				var f = new Users({
+					login: 'dummy', 
+					password: 'password', 
+					numTopic: 0, 
+					numPost: 0, 
+					numComment: 0
+				});
+
 				f.save(function(err) {
-					if(err) {
-						throw err;
+					if (err) {
+						return next(err);
 					} else {
-						console.log('Successfully saved', f);
 						res.json(f);
 					}
 			});
 		},
-		login: function(req, res, app) {
-			console.log('reqested login = ', req.body.login);
+		login: function(req, res, next) {
+
 			Users.findOne({login: req.body.login}, function(err, user) {
-				if(err) {
-					throw err;
+				if (err) {
+					// throwing an error does nothing here, if it's not handled properly
+					// no response is sent to the client
+					return next(err);
 				} else {
-					if(!user) {
-						console.log('user does not exist');
+					if (!user) {
 						res.json({success: false, message: 'user does not exist'});
 					} else {
 						// if login is new create user and return
-						if(user.password != req.body.password) {
-							res.json({success:false, message: 'password is not correct'});
+						if (user.password != req.body.password) {
+							res.json({success: false, message: 'password is not correct'});
 						} else {
 							var token = jwt.sign(user, app.get('superSecret'), {
 								expiresIn: 3600*25 // expires in 24 hours
@@ -46,79 +53,76 @@ module.exports = (function() {
 						}
 					}
 				}
-
 			});
 		},
-		create: function(req, res) {
-			console.log('user create req.body.login, req.body.password', req.body.login, req.body.password);
+		create: function(req, res, next) {
+			// bad practice to console.log passwords.
 			Users.findOne({login: req.body.login}, function(err, result) {
-				if(err) {
-					console.log('error in create of user', err);
-					res.json({success:false, message: err});
+				if (err) {
+					return next(err);
 				} else {
-					if(!result) {
+					if (!result) {
 						// if login is new create user and return
-						var f = new Users({login: req.body.login, password: req.body.password, numTopic: 0, numPost: 0, numComment: 0});
-						console.log('f=', f);
+						var f = new Users({
+							login: req.body.login, 
+							password: req.body.password, 
+							numTopic: 0, 
+							numPost: 0, 
+							numComment: 0
+						});
+				
 						f.save(function(err) {
-							if(err) {
-								console.log('Error in saving');
-								res.json({success:false, message: err});
+							if (err) {
+								return next(err);
 							} else {
-								console.log('Successfully saved', f); 
 								res.json({success:true, message: f});
 							}
 						});
-						
 					} else {
-						console.log('user already exist');
 						res.json({success:false, message: 'user already exist'});
 					}
 				}
 
 			});
 		},
-		check_duplicate: function(req, res) {
-			console.log('user check_duplicate req.body.login', req.body.login);
+		check_duplicate: function(req, res, next) {
+		
 			Users.findOne({login: req.body.login}, function(err, result) {
-				if(err) {
-					console.log('error in create of user', err);
-					res.json({success:false, message: err});
+				if (err) {
+					return next(err);
 				} else {
-					if(!result) {
-						res.json({success:true});
-						console.log('login name does not exist');
+					if (!result) {
+						res.json({success: true});
 					} else {
-						console.log('login name already exist');
 						res.json({success:false, message: 'login already exist'});
 					}
 				}
-
 			});
 		},
-		get: function(req, res) {
-			console.log('users controller req.params.id', req.params.id);
+		get: function(req, res, next) {
+	
 			Users.findOne({_id:req.params.id}, function(err, result) {   // find only one document
-				if(err) {
-				 	console.log(err);
+				if (err) {
+				 	return next(err);
 				} else {
 					// calculate the number of topic, post, comment here
 					Topics.find({_user:req.params.id}, function(err, results) {   // find all documents
-						if(err) {
-						 console.log(err);
+						if (err) {
+							return next(err);
 						} else {
 							result.numTopic = results.length;   // return it as json format
+
 							Post.find({_user:req.params.id}, function(err, results) {   // find all documents
-								if(err) {
-								 console.log(err);
+								if (err) {
+								  return next(err);
 								} else {
 									result.numPost = results.length;   // return it as json format
+
 									Comment.find({_user:req.params.id}, function(err, results) {   // find all documents
-										if(err) {
-										 console.log(err);
+										if (err) {
+											return next(err);
 										} else {
 											result.numComment = results.length;   // return it as json format
-											console.log('users controller result=', result);
 											res.json(result);   // return it as json format											
 										}
 									});									
@@ -126,9 +130,6 @@ module.exports = (function() {
 							});
 						}
 					});
-
-
-
 				}
 			})
 		}		
