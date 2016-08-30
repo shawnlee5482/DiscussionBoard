f1f29d9a388d6058d060e5df2420b20688bfb7ad
Richard Plute <rplute@gmail.com>
2016-08-30 00:12:19 -0700

    refactoring remaining topic services

--- server/controllers/topics.js
+++ server/controllers/topics.js
@@ -17,7 +17,7 @@ module.exports = (function() {
 			    } 
 			  });
 		},
-		down: function(req, res) {
+		down: function(req, res, next) {
 			Post.findOneAndUpdate({ _id: req.params.id }, { $inc: { upCount: -1 } })
 			  .select('upcount')
 			  .exec(function(err, data) { 
@@ -28,114 +28,124 @@ module.exports = (function() {
 			    } 
 			  });
 		},
-		index: function(req, res) {
+		index: function(req, res, next) {
 			Topics.find({})
 			.populate('_user')
 			.populate('_post')  // population of comment is not necessary in the UI
 			.exec(function(err, topics) { 
-				if(err) {
-				 console.log(err);
+				if (err) {
+					return next(err);
 				} else {
 					res.json(topics);
 				}
 			});
 		},
 
-		detailInfo: function(req, res) {
+		detailInfo: function(req, res, next) {
 			Topics.findOne({_id: req.params.id})
 			.populate('_user')
 			.populate('_post')
 			.populate({path: '_post', populate: {path: '_comments', populate: {path: '_user'}}})  // need to populate comments as well		
 			.populate({path: '_post', populate: {path: '_user'}})  // need to populate comments as well
 			.exec(function(err, topic) {
-				if(err) {
-				 console.log(err);
+				if (err) {
+				  return next(err);
 				} else {
-					console.log('topic controller:', req.params.id);
-					console.log('topic controller:', topic);
 					res.json(topic);				
 				}
 			});			
 		},
 
-		addPost: function(req, res) {
+		addPost: function(req, res, next) {
 			Topics.findOne({_id: req.params.id})
 			.populate('_post')
 			.exec(function(err, topic) {   // find all documents and get document by index
-				if(err) {
-				 console.log(err);
+				if (err) {
+				  return next(err);
 				} else {
-					console.log('topic controller:', req.params.id);
-					console.log('topic controller:', topic)
-					console.log('topic controller input parameter:', req.body.postContent, req.body.imageURL, req.body.id);
-					var newPost = new Post({_topic:topic._id, postContent:req.body.postContent, imageURL: req.body.imageURL, _user:req.body.id,
-						comments:[], upCount: 0, downCount:0});
+
+					var newPost = new Post({
+						_topic: topic._id, 
+						postContent: req.body.postContent, 
+						imageURL: req.body.imageURL, 
+						_user: req.body.id,
+						comments: [], 
+						upCount: 0, 
+						downCount:0
+					});
 
 					newPost.save(function(err) {
-						if(err) {
-							console.log('newPost save error');
+						if (err) {
+							return next(err);
 						} else {
+
 							topic._post.push(newPost);
 
 							topic.save(function(err) {
-								if(err) {
-									res.json(err);
+								if (err) {
+									return next(err);
 								} else {
-									console.log('topic controller addPost:', topic._post);
 									res.json(topic._post);
 								}
 							});								
 						}
-
 					});
 				}
 			});
-
 		},	
-		addComment: function(req, res) {
+		addComment: function(req, res, next) {
 			Post.findOne({_id:req.params.id})
 			.populate('_comments')
 			.exec(function(err, post) {   // find all documents and get document by index
-				if(err) {
-				 console.log(err);
+				if (err) {
+				  return next(err);
 				} else {
-					console.log('topic controller addComment', post);
-					console.log('topic controller input parameter:', req.body.topicId, req.body.comment, req.body.userId);
-
-					var newComment = new Comment({_topic:req.body.topicId, _post:post._id, comment:req.body.comment, _user:req.body.userId});
+				
+					var newComment = new Comment({
+						_topic: req.body.topicId,
+						_post: post._id, 
+						comment: req.body.comment, 
+						_user:req.body.userId
+					});
 
 					newComment.save(function(err) {
-						if(err) {
-							console.log('newPost save error');
+						if (err) {
+							return next(err);
 						} else {
 							post._comments.push(newComment);
 							post.save(function(err) {
-								if(err) {
-									console.log('topic controller addComment: Error occured');
+								if (err) {
+									return next(err);
 								} else {
-									console.log('topic controller addComment:', post);
-									return res.json(post);
+									res.json(post);
 								}
 							});								
 						}
-
 					});
 				}
 			});
-
 		},			
-		create: function(req, res) {
-			var f = new Topics({category:req.body.category, topic:req.body.topic, imageURL: req.body.imageURL, _user:req.body.id, description:req.body.description, numPosts: 0, date:req.body.date});
-			console.log('f=', f);
+		create: function(req, res, next) {
+			var f = new Topics({
+				category: req.body.category, 
+				topic: req.body.topic, 
+				imageURL: req.body.imageURL, 
+				_user: req.body.id, 
+				description: req.body.description, 
+				numPosts: 0, 
+				date: req.body.date
+			});
+
 			f.posts = [];
 			f.save(function(err) {
-				if(err) {
-					console.log('Error in saving');
+				if (err) {
+					return next(err);
 				} else {
-					console.log('Successfully saved', f); 		
+					// this goes here; otherwise, create will always send a 200 response
+					// even if the save() failed
+				  res.json(f);	
 				}
 			});
-			res.json(f);
 		}		
 	}
 
